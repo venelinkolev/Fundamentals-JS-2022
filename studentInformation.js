397d3b4a7fa98ce34ceb1570ee1b7e2d187ea907
venelinkolev <venelinkolev@gmail.com>
2022-09-23 17:50:34 +0300

    Create studentInformation.js

---
+++ studentInformation.js
@@ -0,0 +1,9 @@
+function studentInformation(a, b, c) {
+  let studentName = a;
+  let age = b;
+  let grade = c.toFixed(2);
+
+  console.log(`Name: ${studentName}, Age: ${age}, Grade: ${grade}`);
+}
+
+studentInformation("John", 15, 5.5478);

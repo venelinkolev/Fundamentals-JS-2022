ecb952bdf326a8c068976c50d4f772155472ea34
venelinkolev <venelinkolev@gmail.com>
2022-09-23 17:49:32 +0300

    Create excellentGrade.js

---
+++ excellentGrade.js
@@ -0,0 +1,8 @@
+function excellentGrade(a) {
+  let grade = a;
+  grade > 5.0 ? console.log("Excellent") : console.log("Not excellent");
+  //   if (grade >= 5.5) console.log("Excellent");
+  //   else console.log("Not excellent");
+}
+
+excellentGrade(5.5);

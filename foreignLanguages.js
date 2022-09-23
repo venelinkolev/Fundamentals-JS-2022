87115e3794798b891c501a8e86b1107ee74d8468
venelinkolev <venelinkolev@gmail.com>
2022-09-23 17:49:39 +0300

    Create foreignLanguages.js

---
+++ foreignLanguages.js
@@ -0,0 +1,14 @@
+function foreignLanguages(c) {
+  const country = c;
+
+  if (country === "England" || country === "USA") console.log("English");
+  else if (
+    country === "Spain" ||
+    country === "Argentina" ||
+    country === "Mexico"
+  )
+    console.log("Spanish");
+  else console.log("unknown");
+}
+
+foreignLanguages("Mexico");

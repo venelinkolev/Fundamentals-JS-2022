b4b6be60900b027bd1f297e417127a3d9839cb55
Venelin Kolev <venelinkolev@gmail.com>
2022-09-28 23:15:59 +0300

    Reverse in Place

--- reverseInPlace.js
+++ reverseInPlace.js
@@ -1,6 +1,18 @@
 function reverseInPlace (input) {
     let array = input;
+    let temp = 0;
+    let buffer = "";
+    for (let i = 0; i < array.length / 2; i++) {
+        temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        buffer += array[i] + " ";
+    }
+    console.log(buffer);
 
 }
 
-reverseInPlace();
+reverseInPlace(['33', '123', '0', 'dd']);

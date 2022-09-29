2dfd0958ce1647223ca5d6d1299d9fbdfcfa5629
Venelin Kolev <venelinkolev@gmail.com>
2022-09-30 00:18:24 +0300

    Dungeonest Dark

--- dungeonestDark.js
+++ dungeonestDark.js
@@ -2,6 +2,11 @@ function dungeonestDark(arr) {
   let arrayString = arr.toString();
   let arrayRooms = [];
   let lastRoomStart = 0;
+  let arrayRoomsArray = [];
+
+  const initialHealth = 100;
+  let health = 100;
+  let initialCoins = 0;
 
   for (let i = 0; i < arrayString.length; i++) {
     for (let j = i + 1; j < arrayString.length; j++) {
@@ -12,16 +17,76 @@ function dungeonestDark(arr) {
       }
     }
   }
-  arrayRooms.push(arrayString.slice(lastRoomStart, arrayString.length - 1));
 
+  arrayRooms.push(arrayString.slice(lastRoomStart, arrayString.length));
+  
   console.log(arrayRooms);
   console.log(lastRoomStart);
   console.log("-----------");
-  let room1 = arrayString.slice(0, 6);
-  console.log(room1);
-  console.log(arrayString.indexOf("|"));
-  console.log(arrayString.length);
-  console.log(arrayString.charAt(0));
+
+  for (let i = 0; i < arrayRooms.length; i++) {
+    let currentElement = arrayRooms[i];
+    for (let j = 0; j < currentElement.length; j++) {
+      if (currentElement[j] === " ") {
+        arrayRoomsArray.push(currentElement.slice(0, j));
+        arrayRoomsArray.push(currentElement.slice(j + 1, currentElement.length));
+      }
+    }
+  }
+  arrayRoomsArray.push("END");
+
+  console.log(arrayRoomsArray);
+  console.log("---------");
+
+  let index = 0;
+  let counter = 0;
+
+  let currentRoomContains = arrayRoomsArray[index];
+  index++;
+
+  let currentRoomNumber = Number(arrayRoomsArray[index]);
+  index++;
+
+  while (currentRoomContains !== "END") {
+    counter++;
+    switch (currentRoomContains) {
+      case "potion" : 
+      if (health < initialHealth) health += currentRoomNumber;
+      if (health > 100) health = initialHealth;
+      console.log(`You healed for ${currentRoomNumber} hp.`);
+      console.log(`Current health: ${health} hp.`);
+        break;
+      case "chest" :
+        initialCoins += currentRoomNumber;
+        console.log(`You found ${initialCoins} coins.`);
+        break;
+        default :
+        health -= currentRoomNumber;
+        if (health > 0) {
+          console.log(`You slayed ${currentRoomContains}.`);
+        } else {
+          console.log(`You died! Killed by ${currentRoomContains}.`);
+          console.log(`Best room: ${counter}`);
+        }
+    }
+    if (health < 1) break;
+    currentRoomContains = arrayRoomsArray[index];
+    index++;
+
+    currentRoomNumber = Number(arrayRoomsArray[index]);
+    index++;
+  }
+
+  if (currentRoomContains === "END") {
+    console.log("You've made it!");
+    console.log(`Coins: ${initialCoins}`);
+    console.log(`Health: ${health}`);
+  }
+  // let room1 = arrayString.slice(0, 6);
+  // console.log(room1);
+  // console.log(arrayString.indexOf("|"));
+  // console.log(arrayString.length);
+  // console.log(arrayString.charAt(0));
 }
 
 dungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"]);

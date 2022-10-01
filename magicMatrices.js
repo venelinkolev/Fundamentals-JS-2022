69c041072d436d1a0141d8659808e45d93b77e69
venelinkolev <venelinkolev@gmail.com>
2022-10-01 12:49:19 +0300

    Magic Matrix

---
+++ magicMatrices.js
@@ -0,0 +1,63 @@
+function magicMatrices(arr) {
+  const arrayMatrix = arr;
+  const arrayLength = arrayMatrix.length;
+  let isMagic = true;
+  let sumNumber = 0;
+
+  for (let i = 0; i < arrayLength; i++) {
+    let sumRow = 0;
+    let sumColum = 0;
+    for (let j = 0; j < arrayLength; j++) {
+      sumRow += arrayMatrix[i][j];
+      sumColum += arrayMatrix[j][i];
+      console.log(sumColum, "----", sumRow);
+      console.log("--------");
+    }
+    if (sumColum === sumRow) sumNumber = sumColum;
+    if (sumColum !== sumNumber || sumRow !== sumNumber) {
+      isMagic = false;
+      break;
+    }
+    console.log(sumColum, "----", sumRow);
+    console.log("--------");
+    if (sumColum !== sumRow) {
+      isMagic = false;
+      break;
+    }
+  }
+
+  isMagic ? console.log(isMagic) : console.log(isMagic);
+}
+magicMatrices([
+  [1, 1, 1],
+  [1, 2, 1],
+  [1, 1, 1],
+]);
+
+// magicMatrices([
+//   [1, 0, 0],
+//   [0, 0, 1],
+//   [0, 1, 0],
+// ]);
+
+// magicMatrices([
+//   [4, 5, 6],
+
+//   [6, 5, 4],
+
+//   [5, 5, 5],
+// ]);
+
+// magicMatrices([
+//   [0, 0, 0],
+
+//   [0, 0, 0],
+
+//   [0, 0, 0],
+// ]);
+
+// magicMatrices([
+//   [11, 32, 45],
+//   [21, 0, 1],
+//   [21, 1, 1],
+// ]);

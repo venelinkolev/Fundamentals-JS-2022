13625eec46215a040685df768260cb6a3148308f
venelinkolev <venelinkolev@gmail.com>
2022-10-05 16:57:14 +0300

    Spiral Matrix New

--- spiralMatrix.js
+++ spiralMatrix.js
@@ -1,54 +1,53 @@
-function spiralMatrix (dem1, dem2) {
-    let array = Array(dem1);
-    let arrayMatrix = [];
-    let arrayCounter = 1;
-    let row = 0;
-    let column = 0;
-    let rowEnd = dem1 - 1;
-    let columnEnd = dem2 -1;
+function spiralMatrix(dem1, dem2) {
+  let array = Array(dem1);
+  let arrayMatrix = [];
+  let arrayCounter = 1;
+  let row = 0;
+  let column = 0;
+  let rowEnd = dem1 - 1;
+  let columnEnd = dem2 - 1;
 
-    for (let i = 0; i < dem1; i++) {
-        arrayMatrix.push(Array(dem1));
-        for (let j = 0; j < dem2; j++) {
-            arrayMatrix[i][j] = 0;
-        }
+  for (let i = 0; i < dem1; i++) {
+    arrayMatrix.push(Array(dem1));
+    for (let j = 0; j < dem2; j++) {
+      arrayMatrix[i][j] = 0;
     }
+  }
 
-    while (rowEnd > dem1 / 2 || columnEnd > dem2 / 2) {
-    
+  while (rowEnd > dem1 / 2 || columnEnd > dem2 / 2) {
     for (let i = row; i <= rowEnd; i++) {
-        arrayMatrix[row][i] = arrayCounter;
-        arrayCounter++;
+      arrayMatrix[row][i] = arrayCounter;
+      arrayCounter++;
     }
     row++;
 
     for (let i = row; i <= columnEnd; i++) {
-        arrayMatrix[i][columnEnd] = arrayCounter;
-        arrayCounter++;
+      arrayMatrix[i][columnEnd] = arrayCounter;
+      arrayCounter++;
     }
     columnEnd--;
 
     for (let i = columnEnd; i >= column; i--) {
-        arrayMatrix[rowEnd][i] = arrayCounter;
-        arrayCounter++;
+      arrayMatrix[rowEnd][i] = arrayCounter;
+      arrayCounter++;
     }
     rowEnd--;
 
     for (let i = rowEnd; i >= row; i--) {
-        arrayMatrix[i][column] = arrayCounter;
-        arrayCounter++;
-    }    
-    column++;
+      arrayMatrix[i][column] = arrayCounter;
+      arrayCounter++;
     }
+    column++;
+  }
 
-    // for (let i = row; i <= rowEnd; i++) {
-    //     arrayMatrix[row][i] = arrayCounter;
-    //     arrayCounter++;
-    // }
+  // for (let i = row; i <= rowEnd; i++) {
+  //     arrayMatrix[row][i] = arrayCounter;
+  //     arrayCounter++;
+  // }
 
-    
-
-    console.log(arrayMatrix);
+  for (let i = 0; i < arrayMatrix.length; i++) {
+    console.log(arrayMatrix[i]);
+  }
 }
 
 spiralMatrix(10, 10);

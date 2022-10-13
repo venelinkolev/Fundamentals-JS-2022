aa92762d1a92a8090b7e0044da1510e5a0857a62
venelinkolev <venelinkolev@gmail.com>
2022-10-13 17:55:08 +0300

    Bomb Final

--- bombNumbers.js
+++ bombNumbers.js
@@ -1,6 +1,36 @@
 function bombNumbers(arrOne, arrTwo) {
   let targetArray = arrOne;
   let bombArray = arrTwo;
+  let sum = 0;
+
+  let bombNumber = bombArray[0];
+  let detonationNumber = bombArray[1];
+  let index = 0;
+
+  for (let i = 0; i < targetArray.length; i++) {
+    if (targetArray[i] === bombNumber) {
+      index = i;
+
+      targetArray.splice(index + 1, detonationNumber);
+
+      if (index - detonationNumber < 0) {
+        targetArray.splice(0, index + 1);
+      } else {
+        targetArray.splice(index - detonationNumber, detonationNumber + 1);
+      }
+      i = 0;
+    }
+  }
+
+  for (let i of targetArray) {
+    sum += i;
+  }
+
+  console.log(sum);
 }
 
+bombNumbers([1, 1, 2, 1, 1, 1, 2], [2, 3]);
 bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
+bombNumbers([1, 4, 4, 2, 8, 9, 1], [9, 3]);
+bombNumbers([1, 7, 7, 1, 2, 3], [7, 1]);
+bombNumbers([1, 1, 2, 1, 1, 1, 2, 1, 1, 1], [2, 1]);
--- test.js
+++ test.js
@@ -14,3 +14,39 @@ solve(12.8, 1);
 base = transport(base);
 console.log(base);
 //transport(base);
+
+function bombNumbers(array1, array2) {
+  let [bomb, power] = array2;
+  while (array1.includes(bomb)) {
+    array1.splice(Math.max(0, array1.indexOf(bomb) - power), power * 2 + 1);
+  }
+  console.log(array1.reduce((a, b) => a + b, 0));
+}
+
+function bombNumbers(bombField, bombArray) {
+  let bombIndex = null;
+  let power = bombArray[1];
+  let sum = 0;
+  let chargedBomb = bombField.includes(bombArray[0]);
+
+  if (chargedBomb) {
+    // IF there isn't special-bomb-number DO NOTHING
+    while (chargedBomb) {
+      //IF there is more than one bobs
+      bombIndex = bombField.indexOf(bombArray[0]); //Index of special-bomb-number
+      bombField.splice(bombIndex + 1, power); // Removing element on the right side of the bomb excluding bomb
+      if (bombIndex - power < 0) {
+        //IF bombs will deploy elements before 0;
+        bombField.splice(0, bombIndex + 1); //  Removing all elements including bombIndex
+      } else {
+        bombField.splice(bombIndex - power, power + 1); // Starting index , power + bomb
+      }
+      chargedBomb = bombField.includes(bombArray[0]); // check for another bomb
+    }
+    for (let i = 0; i < bombField.length; i++) {
+      sum += bombField[i];
+    }
+    console.log(sum);
+  }
+}
+bombNumbers([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);

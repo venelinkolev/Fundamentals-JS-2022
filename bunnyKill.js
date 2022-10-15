e9992d011efd799814280f7b26aca37e66dabc78
venelinkolev <venelinkolev@gmail.com>
2022-10-15 12:52:24 +0300

    Bunny Kill

--- bunnyKill.js
+++ bunnyKill.js
@@ -1,4 +1,25 @@
-function bunnyKill() {}
+function bunnyKill(arr) {
+  let bunniesArray = arr;
+  let coordinateArray = bunniesArray.pop().split(" ").join().split(",");
+  let length = bunniesArray.length;
+  let bunnyBombValue = 0;
+
+  for (let i = 0; i < length; i++) {
+    bunniesArray[i] = bunniesArray[i].split(" ");
+  }
+
+  for (let i = 0; i < coordinateArray.length; i += 2) {
+    bunnyBombValue = bunniesArray[i][i + 1];
+    console.log(bunnyBombValue);
+
+    for (let j = 0; j < length; j++) {
+      for (let k = 0; k < length; k++) {}
+    }
+  }
+
+  console.log(bunniesArray);
+  console.log(coordinateArray);
+}
 
 bunnyKill([
   "5 10 15 20",

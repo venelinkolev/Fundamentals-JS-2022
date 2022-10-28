aebecaefb472d4070815f3ebcbc4ff78bbd99d6e
venelinkolev <venelinkolev@gmail.com>
2022-10-28 17:15:26 +0300

    Classes

---
+++ classLaptop.js
@@ -0,0 +1,47 @@
+class Laptop {
+  constructor(info, quality) {
+    this.info = info;
+    this.isOn;
+    this.quality = quality;
+    this.price;
+  }
+
+  turnOn() {
+    this.isOn = true;
+    this.quality--;
+    this.price = 800 - info.age * 2 + this.quality * 0.5;
+  }
+
+  turnOff() {
+    this.isOn = false;
+    this.quality--;
+    this.price = 800 - info.age * 2 + this.quality * 0.5;
+  }
+
+  showInfo() {
+    return JSON.stringify(this.info);
+  }
+}
+
+let info = {
+  producer: "Dell",
+
+  age: 2,
+  brand: "XPS",
+};
+
+let laptop = new Laptop(info, 10);
+
+laptop.turnOn();
+
+console.log(laptop.showInfo());
+
+laptop.turnOff();
+
+console.log(laptop.quality);
+
+laptop.turnOn();
+
+console.log(laptop.isOn);
+
+console.log(laptop.price);

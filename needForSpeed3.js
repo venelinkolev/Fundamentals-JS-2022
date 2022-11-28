22b4c5b575c17ef1624c3a3f9eafb6b6b2d73b49
Venelin Kolev <venelinkolev@gmail.com>
2022-11-29 00:20:26 +0200

    Pirates and NFS3

--- needForSpeed3.js
+++ needForSpeed3.js
@@ -1,4 +1,81 @@
-function needForSpeed3(arr) {}
+function needForSpeed3(arr) {
+  let numberCars = Number(arr.shift());
+  let cars = [];
+  let carsStore = {};
+  let tankMaxLiters = 75;
+
+  for (let i = 0; i < numberCars; i++) {
+    let currentCar = arr.shift().split('|');
+    let car = currentCar[0];
+    let mileage = Number(currentCar[1]);
+    let fuel = Number(currentCar[2]);
+
+    carsStore = {car, mileage, fuel};
+    cars.push(carsStore);
+  }
+
+  while (arr[0] !== 'Stop') {
+    let command = arr.shift().split(' : ');
+
+    switch (command[0]) {
+      case 'Drive':
+        let carDrive = command[1];
+        let distance = Number(command[2]);
+        let fuelDrive = Number(command[3]);
+
+        let currentDriveCar = cars.find((i) => i.car === carDrive);
+
+        if (currentDriveCar.fuel >= fuelDrive) {
+          currentDriveCar.mileage += distance;
+          currentDriveCar.fuel -= fuelDrive;
+          console.log(`${carDrive} driven for ${distance} kilometers. ${fuelDrive} liters of fuel consumed.`)
+        } else {
+          console.log('Not enough fuel to make that ride');
+        }
+
+        if (currentDriveCar.mileage > 100000) {
+          let index = cars.indexOf(currentDriveCar);
+          cars.splice(index, 1);
+          console.log(`Time to sell the ${carDrive}!`)
+        }
+        //console.log(currentDriveCar.car);
+        break;
+        case 'Refuel':
+          let carRefuel = command[1];
+          let fuelLiters = Number(command[2]);
+
+          let currentRefuelCar = cars.find((i) => i.car === carRefuel);
+          //console.log(currentRefuelCar);
+          if (currentRefuelCar.fuel + fuelLiters <= tankMaxLiters) {
+            currentRefuelCar.fuel = currentRefuelCar.fuel + fuelLiters;
+            //console.log(currentRefuelCar.fuel);
+            console.log(`${carRefuel} refueled with ${fuelLiters} liters`)
+          } else {
+            console.log(`${carRefuel} refueled with ${tankMaxLiters - currentRefuelCar.fuel} liters`)
+            currentRefuelCar.fuel = tankMaxLiters;
+            //console.log(currentRefuelCar.fuel);
+          }
+          break;
+          case 'Revert':
+            let carRevert = command[1];
+            let kilometers = command[2];
+
+            let currentRevertCar = cars.find((i) => i.car === carRevert);
+            currentRevertCar.mileage -= kilometers;
+            //console.log(currentRevertCar.mileage);
+            if (currentRevertCar.mileage < 10000) {
+              currentRevertCar.mileage = 10000;
+            } else {
+              console.log(`${carRevert} mileage decreased by ${kilometers} kilometers`);
+            }
+            break;
+    }
+  }
+  for (let i of cars) {
+    console.log(`${i.car} -> Mileage: ${i.mileage} kms, Fuel in the tank: ${i.fuel} lt.`)
+  }
+  //console.log(arr);
+}
 
 needForSpeed3([
   '3',
--- pirates.js
+++ pirates.js
@@ -1,4 +1,69 @@
-function pirates(arr) {}
+function pirates(arr) {
+  let targetedCities = [];
+  let cities = {};
+
+  while (arr[0] !== 'Sail') {
+    let currentCity = arr.shift().split('||');
+    let city = currentCity[0];
+    let population = Number(currentCity[1]);
+    let gold = Number(currentCity[2]);
+
+    let findCity = targetedCities.find((i) => i.city === city);
+    if (findCity) {
+      findCity.population += population;
+      findCity.gold += gold;
+    } else {
+      cities = {city , population, gold};
+      targetedCities.push(cities);
+    }
+  }
+  //console.log(targetedCities);
+
+  while (arr[0] !== 'End') {
+    let currentEvent = arr.shift().split('=>');
+    let event = currentEvent[0];
+    let cityEvent = currentEvent[1];
+
+    let findCity = targetedCities.find((i) => i.city === cityEvent);
+
+    switch (event) {
+      case 'Plunder':
+        let people = Number(currentEvent[2]);
+        let plunderGold = Number(currentEvent[3]);
+
+        findCity.population -= people;
+        findCity.gold -= plunderGold;
+        console.log(`${cityEvent} plundered! ${plunderGold} gold stolen, ${people} citizens killed.`);
+
+        if (findCity.population === 0 || findCity.gold === 0) {
+          console.log(`${cityEvent} has been wiped off the map!`);
+          let index = targetedCities.indexOf(findCity);
+          targetedCities.splice(index, 1);
+        }
+        break;
+        case 'Prosper':
+          let prosperGold = Number(currentEvent[2]);
+
+          if (prosperGold > 0) {
+            findCity.gold += prosperGold;
+            console.log(`${prosperGold} gold added to the city treasury. ${cityEvent} now has ${findCity.gold} gold.`);
+          } else {
+            console.log(`Gold added cannot be a negative number!`);
+          }
+          break;
+    }
+  }
+ 
+  if (targetedCities.length === 0) {
+    console.log('Ahoy, Captain! All targets have been plundered and destroyed!');
+  } else {
+    console.log(`Ahoy, Captain! There are ${targetedCities.length} wealthy settlements to go to:`);
+    
+    for (let i of targetedCities) {
+      console.log(`${i.city} -> Population: ${i.population} citizens, Gold: ${i.gold} kg`);
+    }
+  }
+}
 
 pirates([
   'Tortuga||345000||1250',
